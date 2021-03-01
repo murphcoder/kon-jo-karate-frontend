126bf9af24b885dd1b783b1c028ee4f863da4b5f
murphcoder <murphcoder@gmail.com>
2021-02-28 22:23:16 -0600

    Initial layout of router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+         <BrowserRouter>
+          <Switch>
+            <Route exact path='/' />
+            <Route exact path='/classes' />
+            <Route exact path='/locations' />
+            <Route exact path='/instructors' />
+            <Route exact path='/parents' />
+            <Route exact path='/parents/:tripId' />
+            <Route exact path='/instructors/:tripId' />
+            <Route exact path='/login' />
+            <Route exact path='/signup' />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  }
+
+};
 
 export default App;
