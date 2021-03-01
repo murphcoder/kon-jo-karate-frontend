58c1311c3ce1a438b20db4982e7a5e6d61f5025a
murphcoder <murphcoder@gmail.com>
2021-02-28 22:27:42 -0600

    correction

--- src/App.js
+++ src/App.js
@@ -10,13 +10,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' />
             <Route exact path='/classes' />
-            <Route exact path='/locations' />
-            <Route exact path='/instructors' />
-            <Route exact path='/parents' />
-            <Route exact path='/parents/:tripId' />
-            <Route exact path='/instructors/:tripId' />
-            <Route exact path='/login' />
-            <Route exact path='/signup' />
+            
           </Switch>
         </BrowserRouter>
       </div>
