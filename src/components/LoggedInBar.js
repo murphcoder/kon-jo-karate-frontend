50ddc65bab062627048c33f02e5d4b2fe45398e2
murphcoder <murphcoder@gmail.com>
2021-03-07 21:26:59 -0600

    Correction

--- src/components/LoggedInBar.js
+++ src/components/LoggedInBar.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
 
-const logout = (props) => {
-    props.logoutUser(props.url);
-};
-
 const LoggedInBar = (props) => {
     return (
         <div className="menu">
