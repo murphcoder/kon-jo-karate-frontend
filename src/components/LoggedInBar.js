f779a265e79beb3e268b4f48d2535db5b8c50d5f
murphcoder <murphcoder@gmail.com>
2021-03-14 21:29:53 -0500

    log in debugging

--- src/components/LoggedInBar.js
+++ src/components/LoggedInBar.js
@@ -5,6 +5,7 @@ const LoggedInBar = (props) => {
     return (
         <div className="menu">
             <NavLink to="/">Home</NavLink>
+            <NavLink to={{pathname: "/classes"}}>Classes</NavLink>
             <NavLink to={{pathname: "/locations"}}>Locations</NavLink>
             <NavLink to={{pathname: "/instructors"}}>Instructors</NavLink>
             <NavLink className="logout" to="/" onClick={event => logout(props)}>Logout</NavLink>
