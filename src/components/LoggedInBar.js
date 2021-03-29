06966fb4c414700db3c9259bd56612a0d350d409
murphcoder <murphcoder@gmail.com>
2021-03-28 22:22:00 -0500

    expanded

--- src/components/LoggedInBar.js
+++ src/components/LoggedInBar.js
@@ -4,6 +4,11 @@ import {NavLink} from 'react-router-dom';
 const LoggedInBar = (props) => {
     return (
         <div className="menu">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to={{pathname: "/classes"}}>Classes</NavLink>
+        <NavLink to={{pathname: "/locations"}}>Locations</NavLink>
+        <NavLink to={{pathname: "/instructors"}}>Instructors</NavLink>
+        <NavLink className="logout" to="/" onClick={}>Logout</NavLink>
         </div>
     )
 };
